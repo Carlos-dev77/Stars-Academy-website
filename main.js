aeadc3a3b2f282e4c3250a5f6088dc1a175eb9b3
Carlos <carloskipkemboi777@gmail.com>
2026-06-13 15:39:55 +0300

    Added the school life section and other images

--- main.js
+++ main.js
@@ -47,3 +47,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+
+
+const cards = document.querySelectorAll('.life-card');
+
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if(cardTop < window.innerHeight - 100){
+            card.classList.add('show');
+        }
+    });
+});
